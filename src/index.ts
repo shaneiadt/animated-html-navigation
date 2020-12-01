e53342c3bb4a0744249839aa28f575d8fc202da9
Shane O'Moore <shaneiadt@gmail.com>
2020-12-01 16:34:48 +0000

    animate menu bars

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,21 @@ const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
 
+enum SLIDE_DIRECTION {
+    forward,
+    backward
+};
+
+function slide(direction: SLIDE_DIRECTION): void {
+    const remove = direction === SLIDE_DIRECTION.forward ? 'out' : 'in';
+    const add = direction === SLIDE_DIRECTION.forward ? 'in' : 'out';
+
+    [nav1, nav2, nav3, nav4, nav5].forEach((el, i) => {
+        el?.classList.remove(`slide-${remove}-${i + 1}`);
+        el?.classList.add(`slide-${add}-${i + 1}`);
+    });
+}
+
 function toggleNav() {
     menuBars?.classList.toggle('change');
     overlay?.classList.toggle('overlay-active');
@@ -13,9 +28,13 @@ function toggleNav() {
     if (overlay?.classList.contains('overlay-active')) {
         overlay.classList.add('overlay-slide-right');
         overlay.classList.remove('overlay-slide-left');
+
+        slide(SLIDE_DIRECTION.forward);
     } else {
         overlay?.classList.add('overlay-slide-left');
         overlay?.classList.remove('overlay-slide-right');
+
+        slide(SLIDE_DIRECTION.backward);
     }
 }
 
